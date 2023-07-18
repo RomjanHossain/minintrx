5775e47d2a6e97dd9d7e1b9caf19e990194a7468
RomjanHossain <romjanhossain726526@gmail.com>
2023-07-18 07:24:56 +0600

    removed migrations and only the necessary file uploaded

--- landing/models.py
+++ landing/models.py
@@ -10,7 +10,64 @@ class NewUser(AbstractUser):
     country = models.CharField(max_length=50, blank=True, null=True)
     refferal = models.CharField(max_length=50, blank=True, null=True)
     balance = models.FloatField(default=0.0)
-    reffer_code = models.UUIDField(default=uuid.uuid4, editable=False)
+    # reffer_code = models.UUIDField(default=uuid.uuid4, editable=False)
+    reffer_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def generate_referral_code(self):
+        return str(uuid.uuid4()).replace("-", "")[
+            :10
+        ]  # Generate a 10-character referral code
+
+    # def save(self, *args, **kwargs):
+    #     if not self.reffer_code:  # Check if referral code is not already set
+    #         self.reffer_code = self.generate_referral_code()
+
+    #     with transaction.atomic():
+    #         # Save the NewUser instance first
+    #         super().save(*args, **kwargs)
+
+    #         if self.refferal:
+    #             print("the refferal id ->", self.refferal)
+    #             reffered_by = NewUser.objects.get(reffer_code=self.refferal)
+
+    #             # Update the referrer's balance
+    #             reffered_by.balance += 10
+    #             reffered_by.save()
+
+    #             # Create and save the RefferedModel instance
+    #             RefferedModel.objects.create(
+    #                 reffered_by=reffered_by, reffered_to=self, amount=0.5
+    #             )
+
+    #             # Update the current user's balance
+    #             self.balance += 5
+    #             self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.reffer_code:  # Check if referral code is not already set
+            self.reffer_code = self.generate_referral_code()
+        if self.refferal:
+            print("this is referred code -> ", self.refferal)
+            reffered_by = NewUser.objects.get(reffer_code=self.refferal)
+            reffered_by.balance += 10
+            reffered_by.save()
+
+            # Update this user balance
+            self.balance += 5
+            super(NewUser, self).save(*args, **kwargs)  # Save the instance first
+
+            # Now add this to the referred model after the instance has been saved
+            rm = RefferedModel.objects.create(
+                reffered_by=reffered_by,
+                reffered_to=self,
+                amount=0.5,
+                reffer_code=self.reffer_code,
+            )
+            rm.save()
+        else:
+            super(NewUser, self).save(
+                *args, **kwargs
+            )  # Save the instance without changes
 
     def __str__(self):
         return self.username
@@ -160,3 +217,25 @@ class PackagePurchase(models.Model):
 
     class Meta:
         verbose_name_plural = "Package Purchase"
+
+
+# reffer code where each reffer the user will get some amount
+class RefferedModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    # user = models.ForeignKey(NewUser, on_delete=models.CASCADE)
+    reffer_code = models.CharField(max_length=1000)
+    # reffered_user = models.ManyToManyField(NewUser, related_name="reffered_user")
+    reffered_by = models.ForeignKey(
+        NewUser, related_name="reffered_by", on_delete=models.CASCADE
+    )
+    reffered_to = models.ForeignKey(
+        NewUser, related_name="reffered_to", on_delete=models.CASCADE
+    )
+    amount = models.FloatField(default=0.0)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.reffered_by.username
+
+    class Meta:
+        verbose_name_plural = "Reffered Code"
